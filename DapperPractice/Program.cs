8204d92854c2bed239353dc33c9dab9515f222d3
amritdumre10 <amritdumre10@gmail.com>
2022-06-23 19:49:48 +0545

    default home screen changes

--- DapperPractice/Program.cs
+++ DapperPractice/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Student}/{action=Index}/{id?}");
 
 app.Run();
